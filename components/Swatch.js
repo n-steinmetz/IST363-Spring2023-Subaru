6d94c1b849ceaa453b842ea428d21f4d6be7bb17
Nick Steinmetz <nsteinme@syr.edu>
2023-03-23 18:19:15 -0400

    Started color picker

---
+++ components/Swatch.js
@@ -0,0 +1,7 @@
+// import styles from ./ swatch.module.scss
+
+const Swatch = ({color}) => {
+    const { name, hex } = color;
+    return <li>{name}</li>
+}
+export default Swatch
--- pages/index.js
+++ pages/index.js
@@ -2,43 +2,41 @@ import Button from '../components/Button';
 import Header from '../components/Header';
 import Layout from '../components/Layout';
 import Image from 'next/image';
+import Swatch from '../components/Swatch'
 
-import { getAllVehicles } from '../lib/api.js'
+import { getAllVehicles, getAllColors } from '../lib/api.js'
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
   const data = getAllVehicles();
+  const colors = getAllColors();
 
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props: {
-      data
+      data,
+      colors,
     }
   }
 }
 
-const Homepage = ({data}) => {
-  console.log({data})
+const Homepage = ({data, colors}) => {
+  
+  // console.log({data})
   return <Layout>
     <h1>Homepage</h1>
-    <Button 
-      label="Register now" 
-      type="primary" 
-    />
-    {data.map((car) => {
-        const {model, slug } = car;
-        return <article>
-          <Image 
-            src={`/vehicles/${slug}/medium.webp`}
-            alt={`${model} car`}
-            width={350}
-            height={185}
-          />
-          <h2>{model}</h2>
-        </article>
-    })}
-
+    <h2>Color Picker</h2>
+    <div>
+      Large Image Here
+    </div>
+    <ul>
+      {colors.map((color) => {
+        
+        return <Swatch color = {color} />
+      })}
+    </ul>
+    <h3>Color name</h3>
   </Layout>
 }
 export default Homepage
